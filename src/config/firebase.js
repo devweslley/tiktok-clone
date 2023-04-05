e1ab5bc11ae88ef99621a88c1c0e09cbd3deba6c
Weslley Sobrinho <wszeroum@gmail.com>
2023-04-05 16:06:17 -0300

    ...

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,22 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 
 const firebaseConfig = {
+  apiKey: "AIzaSyC1UL9dHlJyu8cghWJI6KS6shuMrx2zSsU",
+  authDomain: "tiktok---jclone.firebaseapp.com",
+  projectId: "tiktok---jclone",
+  storageBucket: "tiktok---jclone.appspot.com",
+  messagingSenderId: "198338198417",
+  appId: "1:198338198417:web:08f05dfaa974f1df4eb830"
+};
+
+/*const firebaseConfig = {
   apiKey: "apiKey",
   authDomain: "authDomain",
   projectId: "projectId",
   storageBucket: "storageBucket",
   messagingSenderId: "messagingSenderId",
   appId: "appId"
-};
+}; */
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
