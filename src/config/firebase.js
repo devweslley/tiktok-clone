cb17e094faca6545f87b283868234069c6293192
Weslley Sobrinho <wszeroum@gmail.com>
2023-04-05 15:25:19 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,15 +12,6 @@ const firebaseConfig = {
 };
 */
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC1UL9dHlJyu8cghWJI6KS6shuMrx2zSsU",
-  authDomain: "tiktok---jclone.firebaseapp.com",
-  projectId: "tiktok---jclone",
-  storageBucket: "tiktok---jclone.appspot.com",
-  messagingSenderId: "198338198417",
-  appId: "1:198338198417:web:08f05dfaa974f1df4eb830"
-};
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
