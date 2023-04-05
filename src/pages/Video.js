d09e13ef0933d6f829ba469d588ca4a5fd9d0a96
Weslley Sobrinho <wszeroum@gmail.com>
2023-04-05 14:11:42 -0300

    Update Video.js

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import VideoFooter from './components/footer/VideoFooter'
 import VideoSidebar from './components/sidebar/VideoSidebar'
+import { debounce } from '@mui/material'
 import "./video.css"
 
 function Video({likes, messages, shares, name, description, music, url}) {
@@ -16,6 +17,24 @@ function Video({likes, messages, shares, name, description, music, url}) {
             setPlay(true)
         }
     }
+
+    useEffect(() => {
+    const handleScrollDebounced = debounce(() => {
+      if (videoRef.current) {
+        videoRef.current.currentTime = 0; // Define o tempo de reprodução para 0 segundos
+        videoRef.current.pause(); // Pausa a reprodução do vídeo
+        //setPlay(false); // Atualiza o estado do botão de reprodução
+      }
+    }, 800);
+  
+    window.addEventListener("keydown", handleScrollDebounced);
+    window.addEventListener("wheel", handleScrollDebounced); // Adiciona o ouvinte de evento para o evento "keydown"
+  
+    return () => {
+      window.removeEventListener("keydown", handleScrollDebounced);
+      window.addEventListener("wheel", handleScrollDebounced); // Remove o ouvinte de evento quando o componente é desmontado
+    };
+  }, []);
     
     return (
         <div className='video'>
